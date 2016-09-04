07e2c218c1523fed0fbedf8756eb21248d504d0c
ochoa <ochoa@DESKTOP-8V080EH.attlocal.net>
2016-09-04 09:08:12 -0700

    Prints out message.

---
+++ FirstClass/src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+// main method begins the program.
+public static void main(String[] args)
+{
+	System.out.println("Welcome to Java Programming!");
+} // end method main
+} // end MyFirstClass
+
+
